0a620d9583aead3e7af7e7476109644b78903504
Jon Dawson <dawson.jon@hotmail.com>
2021-11-05 11:43:03 +0000

    first push

---
+++ readme.md
@@ -0,0 +1,46 @@
+# Ski Jump 
+
+## Intro 
+This project was born from my Three.js journey. In this course I come across a library called `cannon.js` to use physics in a three.js scene. Loaded with new knowledge and curiousity, I wanted to create my own project which encapsulates physics and 3D graphics. 
+
+My goal is to create a fun game where the player starts at the top of ski slope and has to manoeuvre between the trees and hit the ski jump at the end successfully. Turning and hitting trees will reduce speed and the distance cleared on launching from ski jump, thus reducing game points. 
+
+It's an ambitous project with a lot of hurdles to overcome!
+
+## Production Walkthrough 
+
+### Create a slope 
+
+**Goal**
+Create a plane which faces the camera head on and inclines. 
+
+**Solution**
+This was relatively simple: <br>
+- Rectangular plane `15 x 20`
+- Move camera higher up `camera.position.y = 12`
+- Zoom camera out `camera.position.z = 6`
+- Adjust angle of floor plane in 3D world and physics would by -0.02
+
+### Ball to roll down slope 
+
+**Goal**
+Implement physics to scene and test with a ball rolling down the slope.
+
+**Solution**
+- Create a ball in 3D and physics world 
+- Give the ball mass
+- Reduce gravity in `CANNON.World()` to -189.82
+- Gravitional pull will mean ball rolls down the slope
+- If gravity was 0 the ball would be static 
+- Move ball away from camera and to top of ski slope (at launch) `sphere.position.z = -7`
+
+Boom! ball is at top of slope and rolls down towards camera. 
+
+### Camera follows ball
+
+**Goal** 
+Currently, the game ski slope is short and thus, the game will be short. The camera is fixed in a single position so if I make the skip slope longer, the player won't be able to see the skier when it's out of view. 
+
+I need to extend the length of the plane and get the camera to follow the ball as it rolls down. 
+
+This ball will eventually become a 3D skier model or a sledge - see how complex the modelling is for both options. Main thing is to get the physics working. # ski-jumping-game
---
+++ src/slope.js
@@ -0,0 +1,210 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as dat from 'dat.gui'
+import CANNON from 'cannon'
+
+// console.log(CANNON)
+
+// GUI
+const gui = new dat.GUI()
+const debugObject = {}
+debugObject.createSphere = () => {
+    createSphere(
+        Math.random() * 0.5,
+        {
+          x: (Math.random() - 0.5) * 3,
+          y: 3,
+          z: (Math.random() - 0.5) * 3 
+        }
+    )
+}
+gui.add(debugObject, 'createSphere')
+
+const canvas = document.querySelector('canvas.webgl')
+const scene = new THREE.Scene()
+
+// Textures 
+const textureLoader = new THREE.TextureLoader()
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+    '/textures/environmentMaps/0/px.png',
+    '/textures/environmentMaps/0/nx.png',
+    '/textures/environmentMaps/0/py.png',
+    '/textures/environmentMaps/0/ny.png',
+    '/textures/environmentMaps/0/pz.png',
+    '/textures/environmentMaps/0/nz.png'
+])
+
+// Physics 
+
+// World 
+const world = new CANNON.World()
+world.gravity.set(0, 0, 0) // Vec3 Class is same as Vector3 but for physics
+
+
+// What happens when plastic collides with concrete 
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial, 
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7
+  }
+)
+world.addContactMaterial(defaultContactMaterial)
+world.defaultContactMaterial = defaultContactMaterial
+
+
+// Floor
+const floorShape = new CANNON.Plane()
+const floorBody = new CANNON.Body()
+// floorBody.material = defaultContactMaterial
+floorBody.mass = 0
+floorBody.addShape(floorShape)
+floorBody.quaternion.setFromAxisAngle(
+  new CANNON.Vec3(-1, 0, 0),
+  Math.PI * 0.5 - 0.02
+)
+world.addBody(floorBody)
+
+
+// Floor 
+const floor = new THREE.Mesh(
+    new THREE.PlaneGeometry(15, 20), // width / height 
+    new THREE.MeshStandardMaterial({
+        color: '#777777',
+        metalness: 0.3,
+        roughness: 0.4,
+        envMap: environmentMapTexture
+    })
+)
+floor.receiveShadow = true
+floor.rotation.x = - Math.PI * 0.5 - 0.02
+scene.add(floor)
+
+// Lights 
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.7)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
+directionalLight.castShadow = true
+directionalLight.shadow.mapSize.set(1024, 1024)
+directionalLight.shadow.camera.far = 15
+directionalLight.shadow.camera.left = - 7
+directionalLight.shadow.camera.top = 7
+directionalLight.shadow.camera.right = 7
+directionalLight.shadow.camera.bottom = - 7
+directionalLight.position.set(5, 5, 5)
+scene.add(directionalLight)
+
+// Sizes 
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+// Camera 
+const camera = new THREE.PerspectiveCamera(100, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0, 12, 6)
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+// Renderer 
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+// Utils 
+const objectsToUpdate = []
+
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20)
+const sphereMaterial = new THREE.MeshStandardMaterial({
+    metalness: 0.3,
+    roughness: 0.4,
+    envMap: environmentMapTexture
+})
+
+
+const createSphere = (radius, position) => {
+
+    // THREE.js mesh 
+    const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+    mesh.scale.set(radius, radius, radius)
+    mesh.castShadow = true 
+    mesh.position.copy(position)
+    scene.add(mesh)
+
+    // Cannon.js body 
+    const shape = new CANNON.Sphere(radius)
+    const body = new CANNON.Body({
+        mass: 1,
+        position: new CANNON.Vec3(0, 3, 0),
+        shape: shape,
+        material: defaultMaterial,
+    })
+    body.position.copy(position)
+    world.addBody(body)
+
+    // Save in objectsToUpdate array
+    objectsToUpdate.push({
+      mesh: mesh,
+      body: body 
+    })
+}
+
+createSphere(0.5, {x: 0, y: 2, z: -7})
+
+// Animate
+const clock = new THREE.Clock()
+let oldElapsedTime = 0 
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - oldElapsedTime
+    oldElapsedTime = elapsedTime
+
+    // Update physics world 
+    world.step(1 / 60, deltaTime, 3) // fixed time stamp, time elapsed, catch up for delay
+
+    for(const object of objectsToUpdate)
+    {
+      object.mesh.position.copy(object.body.position)
+    }
+    
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
